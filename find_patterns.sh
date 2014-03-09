9a0fae7110be11adf6dc37ae4db78350fd2b0c74
yigh <yizhou12345@gmail.com>
2014-03-09 13:32:33 -0500

    dict

--- find_patterns.sh
+++ find_patterns.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ##### ECLAT parameters ###
-MIN_PATTERN_LENGTH=3
-MIN_PATTERN_SUPPORT=4 #Absolute. Can be configured to be percentage
+MIN_PATTERN_LENGTH=2
+MIN_PATTERN_SUPPORT=7 #Absolute. Can be configured to be percentage
+MAX_PATTERN_LENGTH=2
 ##########################
 USAGE="./find_patterns.sh <input_folder> <output_folder>"
 if [ "$#" -ne 2 ]; then
@@ -16,5 +17,5 @@ OUTPUT_DIR=$2/
 mkdir .intermediates
 ./parse.sh $INPUT_DIR .intermediates
 python $PATTERN_MINER .intermediates $OUTPUT_DIR/eclat
-$ECLAT_PATH -m$MIN_PATTERN_LENGTH -s-$MIN_PATTERN_SUPPORT $OUTPUT_DIR/eclat $OUTPUT_DIR/eclat_patterns
+$ECLAT_PATH -n$MAX_PATTERN_LENGTH -m$MIN_PATTERN_LENGTH -s-$MIN_PATTERN_SUPPORT $OUTPUT_DIR/eclat $OUTPUT_DIR/eclat_patterns
 rm -rf .intermediates
